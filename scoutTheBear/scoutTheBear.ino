517c54916276b103fa603fc8503ccd3b163e6583
Il'ya Allen <il.yaallen2008@yahoo.com.au>
2024-02-08 14:50:27 +1300

    Create scoutTheBear.ino

---
+++ scoutTheBear/scoutTheBear.ino
@@ -0,0 +1,39 @@
+const byte LEDPIN = 13 const byte PAWPIN1 = 1;
+const byte PAWPIN2 = 2;
+const byte PAWPIN3 = 3;
+const byte PAWPIN4 = 4;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LEDPIN, OUTPUT);
+  pinMode(PAWPIN1, OUTPUT);
+  pinMode(PAWPIN2, OUTPUT);
+  pinMode(PAWPIN3, OUTPUT);
+  pinMode(PAWPIN4, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(PAWPIN1) == HIGH) {
+    digitalWrite(LEDPIN, HIGH)
+  }
+  if (digitalRead(PAWPIN2) == HIGH) {
+    digitalWrite(LEDPIN, HIGH)
+  }
+  if (digitalRead(PAWPIN3) == HIGH) {
+    playMusic();
+  }
+  if (digitalRead(PAWPIN4) == HIGH) {
+    int score = playGame();
+  }
+}
+
+void playMusic() {
+  // Play an mp3 file
+}
+
+void playGame() {
+  byte score = 99;
+
+  return score
+}
