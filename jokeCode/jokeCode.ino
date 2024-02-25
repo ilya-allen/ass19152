67178142b5e80f595145704f597bb703c3d5c137
Il'ya Allen <il.yaallen2008@yahoo.com.au>
2024-02-26 10:11:13 +1300

    Create jokeCode.ino

---
+++ jokeCode/jokeCode.ino
@@ -0,0 +1,11 @@
+// Im sorry I wasnt sure how to define the library or where coding examples were however this has library added.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
