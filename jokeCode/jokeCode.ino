431455166b68164888d1f390960fab4c9889ab26
David <56525307+Mr-Hansen-Orewa@users.noreply.github.com>
2024-02-27 15:32:43 +1300

    Update jokeCode.ino
    added a teacher comment

--- jokeCode/jokeCode.ino
+++ jokeCode/jokeCode.ino
@@ -1,5 +1,5 @@
 // Im sorry I wasnt sure how to define the library or where coding examples were however this has library added.
-
+//HAN COMMENT we will click on tools menu and then manage libraries and search for the openlog library
 void setup() {
   // put your setup code here, to run once:
 
