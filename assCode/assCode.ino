131df130b36277ea6e88a9dfe8f557aa09b36620
Il'ya Allen <il.yaallen2008@yahoo.com.au>
2024-03-11 10:14:49 +1300

    assCode

---
+++ assCode/assCode.ino
@@ -0,0 +1,17 @@
+/* assesment code */
+
+#include <Wire.h>
+#include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
+OpenLog sdCard; //Create instance
+
+const String FILENAME = "joke.txt";
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
