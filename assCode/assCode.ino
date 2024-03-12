10481f7395217e3e0fb5a5eccfcdbb65f99912cc
Il'ya Allen <il.yaallen2008@yahoo.com.au>
2024-03-13 12:56:36 +1300

    New Function created.
    In this update I created a new function that prints the lines while taking parameters from pressure value and temp values.

--- assCode/assCode.ino
+++ assCode/assCode.ino
@@ -1,17 +1,29 @@
 /* assesment code */
-
 #include <Wire.h>
 #include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
+#include <SparkFun_LPS25HB_Arduino_Library.h>
 OpenLog sdCard; //Create instance
 
+int linesCreated = 0;
 const String FILENAME = "joke.txt";
 
 void setup() {
-  // put your setup code here, to run once:
+  Wire.begin();
+  sdCard.begin();
+
+  sdCard.append("tempAndPressure.txt");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void newLine(pressureVal, tempVal) {
+  sdCard.println("It has been " + (millis() / 1000) " seconds since the board has turned on");
+  sdCard.println("The pressure value is: " + pressureVal);
+  sdCard.println("The temperature value is: " + tempVal);
+  sdCard.syncFile();
+  return linesCreated += 3;
 }
