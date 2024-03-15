7178b65c576cbe71ab9e61f86fbc778de3f64aa7
Il'ya Allen <85529995+ilya-allen@users.noreply.github.com>
2024-03-15 13:10:39 +1300

    Variables added to track temp and pressure
    New Variables have been added to the code loop so that they can track temp and pressure

--- assCode/assCode.ino
+++ assCode/assCode.ino
@@ -3,20 +3,31 @@
 #include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
 #include <SparkFun_LPS25HB_Arduino_Library.h>
 OpenLog sdCard; //Create instance
+LPS25HB pressureSensor;
 
 int linesCreated = 0;
-const String FILENAME = "joke.txt";
+int pressureValue = 0;
+int tempValue = 0;
+const String FILENAME = "tempAndPressure.txt";
 
 void setup() {
   Wire.begin();
   sdCard.begin();
+  
 
-  sdCard.append("tempAndPressure.txt");
+  sdCard.append(FILENAME);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  pressureValue = pressureSensor.getPressure_hPa();
+  tempValue = pressureSensor.getTemperature_degC();
+  
+
+  if(millis() % 100 == 0) {
+    newLine(pressureValue, tempValue)
+  }
+
 
 }
 
@@ -25,5 +36,5 @@ void newLine(pressureVal, tempVal) {
   sdCard.println("The pressure value is: " + pressureVal);
   sdCard.println("The temperature value is: " + tempVal);
   sdCard.syncFile();
-  return linesCreated += 3;
+  return linesCreated += 3;`
 }
