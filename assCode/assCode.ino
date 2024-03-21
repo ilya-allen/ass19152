725aa1234f2ede79648005952ef0e6505e0a707a
Il'ya Allen <85529995+ilya-allen@users.noreply.github.com>
2024-03-21 15:17:01 +1300

    New Comments
    Updated with new comments

--- assCode/assCode.ino
+++ assCode/assCode.ino
@@ -1,15 +1,23 @@
 /* assesment code */
+
+// Non-Core Libraries used for external functions
 #include <Wire.h>
 #include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
 #include <SparkFun_LPS25HB_Arduino_Library.h>
-OpenLog sdCard; //Create instance
+
+// Creates instances like variables so they can be used with the external libraries
+OpenLog sdCard; 
 LPS25HB pressureSensor;
 
+// Integer variables (Changeable variables)
 int linesCreated = 0;
 int pressureValue = 0;
 int tempValue = 0;
+
+// Constant Variables (Non-Changeable)
 const String FILENAME = "tempAndPressure.txt";
 
+// Setup for the csv file so that it is named and ready to receive information
 void setup() {
   Wire.begin();
   sdCard.begin();
@@ -19,6 +27,9 @@ void setup() {
 
 }
 
+
+// This is the main loop containing code  that will take in values realtime and send updates to the
+// csv file every 100ms using the divisible operator.
 void loop() {
   pressureValue = pressureSensor.getPressure_hPa();
   tempValue = pressureSensor.getTemperature_degC();
@@ -31,8 +42,10 @@ void loop() {
 
 }
 
+// This function will be called every time the  milliseconds is a multiple of 100 and instead of constantly 
+// sending multiple print lines we can save coding space by calling a function with input into the parameters
 void newLine(pressureVal, tempVal) {
-  sdCard.println("It has been " + (millis() / 1000) " seconds since the board has turned on");
+  sdCard.println("It has been " + (millis() / 10S00) " seconds since the board has turned on");
   sdCard.println("The pressure value is: " + pressureVal);
   sdCard.println("The temperature value is: " + tempVal);
   sdCard.syncFile();
