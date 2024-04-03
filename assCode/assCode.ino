90e7a03564ab1ca80f708d8481eb3aa830c2e606
Il'ya Allen <85529995+ilya-allen@users.noreply.github.com>
2024-04-03 19:26:10 +1300

    More Comments

--- assCode/assCode.ino
+++ assCode/assCode.ino
@@ -28,6 +28,7 @@ void setup() {
 
 // This is the main loop containing code  that will take in values realtime and send updates to the
 // csv file every 100ms using the divisible operator.
+// Functionality implication
 void loop() {
   pressureValue = pressureSensor.getPressure_hPa();
   tempValue = pressureSensor.getTemperature_degC();
