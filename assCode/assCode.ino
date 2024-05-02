7c3dd41ff604847baffaa114d2c585da90dfd859
Il'ya Allen <85529995+ilya-allen@users.noreply.github.com>
2024-05-03 11:09:34 +1200

    updated and fixed void setup looping error

--- assCode/assCode.ino
+++ assCode/assCode.ino
@@ -1,69 +1,73 @@
 /* assesment code */
-// Code worked upon on 25/4 however this was done on external factors not the code
+
 
 // Non-Core Libraries used for external functions
 #include <Wire.h>
-#include "SparkFun_Qwiic_OpenLog_Arduino_Library.h"
+#include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
 #include <SparkFun_LPS25HB_Arduino_Library.h>
 
+
 // Creates instances like variables so they can be used with the external libraries
-OpenLog sdCard; 
-LPS25HB pressureSensor;
+OpenLog sdCard;
+LPS25HB valSensor;
+
 
 // Changing variables (Changeable variables)
 int linesCreated = 0;
 int pressureValue = 0;
 int tempValue = 0;
 
+
 // Constant Variables (Non-Changeable)
-const String FILENAME = "newTest.txt";
+const String FILENAME = "tempAndPressure.txt";
+
 
 // Setup for the csv file so that it is named and ready to receive information
 void setup() {
-  Wire.begin();
-  sdCard.begin();
-  pressureSensor.begin();
-  sdCard.create(FILENAME);
-  sdCard.append(FILENAME);
-  sdCard.syncFile();
-  Serial.begin(9600);
-//TEACHER COMMENTS I might suggest some checks on if the sdCard and pressure sensor have been turned on correctly go here
-  sdCard.println("new test file");
-  Serial.println("yurr");
+ 
 
+
+  Wire.begin();
+  sdCard.begin(); //Open connection to OpenLog (no pun intended)
+  valSensor.begin();
+  sdCard.append("tempAndPressureValues.txt");
+
+
+  sdCard.println("This is Il'ya's File");
+  sdCard.syncFile();
 }
+
+
 
 
 // This is the main loop containing code  that will take in values realtime and send updates to the
+// csv file every 100ms using the divisible operator.
 // Functionality implication
 void loop() {
-  pressureValue = pressureSensor.getPressure_hPa();
-  tempValue = pressureSensor.getTemperature_degC();
-  
-  // The csv file is updated with a new line every 100ms using the divisible operator through the if statement.
-  if(millis() % 100 == 0) {
-  
-  // TESTING 
-    newLine(pressureValue, tempValue);
-    sdCard.println("testing");
-  }
+  pressureValue = valSensor.getPressure_hPa();
+  tempValue = valSensor.getTemperature_degC();
+ 
+  sdCard.print("yo");
 
-  sdCard.syncFile();
+
+  newLine(pressureValue, tempValue);
+
+
 
 
 }
 
-//TEACHER COMMENTS check what the csv file should look like with the example on classroom - you might keep this one for error checking and have another that 
-//is more suitable for writing to the file
-// This function will be called every time the  milliseconds is a multiple of 100 and instead of constantly 
+
+// This function will be called every time the  milliseconds is a multiple of 100 and instead of constantly
 // sending multiple print lines we can save coding space by calling a function with input into the parameters
 void newLine(int pressure, int temp) {
   String lineIntro[] = {"The amount of seconds since the board has turned on is: ", "The pressure value is: ", "The temperature value is: "};
   int lineValues[] = {millis(), pressure, temp};
-  
+ 
   for (int i = 0; i <= 3; ++i) {
     sdCard.println(lineIntro[i] + lineValues[i]);
   }
   sdCard.syncFile();
-  linesCreated += 3;
+  return linesCreated += 3;
 }
+
